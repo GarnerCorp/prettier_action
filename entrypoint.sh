3ed0e1f3afe7ddae691bbe077a2e8d9fc9066af9
Vlad <vlad.mandrychenko@garnercorp.com>
2020-03-05 13:58:24 -0500

    Clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
-#!/bin/sh
-# e is for exiting the script automatically if a command fails, u is for exiting if a variable is not set
-# x would be for showing the commands before they are executed
-set -eu
+#!/bin/sh -e
 
-# PROGRAM
-echo "Installing prettier..."
 npm install --silent --global prettier
 
-echo "Prettifing files..."
+echo
 prettier $INPUT_PRETTIER_OPTIONS
